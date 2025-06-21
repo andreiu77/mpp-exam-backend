013fddb232ee8f397d02d7fa10d743dc7f26ed96
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-06-21 14:45:38 +0300

    voters done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const authRouter = require("./auth");
 const { getCandidates, setCandidates, pushCandidate } = require("./candidatesStore");
 const { startThread, stopThread } = require("./thread");
